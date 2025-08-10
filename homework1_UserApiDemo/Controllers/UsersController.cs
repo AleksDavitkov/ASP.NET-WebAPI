0908a30aa63bdd7e94049cc9b2d806bde4f828ff
AleksDavitkov <aleksdavitkov@hotmail.com>
2025-08-11 00:42:21 +0200

    01

---
+++ homework1_UserApiDemo/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework1_UserApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAllUsers()
+        {
+            if (UserDb.Users == null || !UserDb.Users.Any())
+            {
+                return NotFound("No users found.");
+            }
+            return Ok(UserDb.Users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetUser(int id)
+        {
+            if (id < 0 || id >= UserDb.Users.Count)
+                return NotFound("User not found.");
+
+            return Ok(UserDb.Users[id]);
+        }
+    }
+}
---
+++ homework1_UserApiDemo/UserDb.cs
@@ -0,0 +1,14 @@
+﻿namespace homework1_UserApiDemo
+{
+    public static class UserDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "PixelNinja",
+            "CodeWizard42",
+            "NeonFlash",
+            "CyberJedi0",
+            "BoldMarker"
+        };
+    }
+}
